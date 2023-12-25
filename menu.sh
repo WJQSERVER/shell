1d847582f6f78ff0a7715c2a4e45c9094c348867
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2023-12-25 23:04:24 +0800

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "请选择要执行的脚本:"
+echo "1. 部署 speedtest-x"
+echo "2. 部署 Docker"
+echo "3. 退出"
+
+read choice
+
+case $choice in
+  1)
+    echo "正在执行部署 speedtest-x 的脚本..."
+    ./deploy_speedtest_x.sh
+    ;;
+  2)
+    echo "正在执行部署 Docker 的脚本..."
+    ./deploy_docker.sh
+    ;;
+  3)
+    echo "退出脚本"
+    exit 0
+    ;;
+  *)
+    echo "无效的选择"
+    exit 1
+    ;;
+esac
+
+echo "部署完成！"
