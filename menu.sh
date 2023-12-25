045fdec361eef126bdf5bf5b693a692e78aeac49
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2023-12-25 23:57:36 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# By WJQSERVER-STUDIO_WJQSERVER
+#https://github.com/WJQSERVER/shell
+#彩色
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
 
 # 更新软件包
 sudo apt update
