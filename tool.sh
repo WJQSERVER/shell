037e93e5056ac59e5b4c95f48bd21bd0633a372e
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2023-12-26 01:42:50 +0800

    Update and rename tool to tool.sh

--- tool.sh
+++ tool.sh
@@ -18,13 +18,20 @@ script3() {
     # 在这里添加脚本3的执行逻辑
 }
 
+# 显示主菜单
+show_main_menu() {
+    echo "请选择要执行的操作:"
+    echo "1. 进入脚本选择菜单"
+    echo "Q. 退出"
+}
+
 # 显示脚本选择菜单
-show_menu() {
+show_script_menu() {
     echo "请选择要执行的脚本:"
     echo "1. 脚本1"
     echo "2. 脚本2"
     echo "3. 脚本3"
-    echo "Q. 退出"
+    echo "B. 返回主菜单"
 }
 
 # 执行选择的脚本
@@ -39,9 +46,8 @@ execute_script() {
         3)
             script3
             ;;
-        Q|q)
-            echo "退出脚本选择界面。"
-            exit 0
+        B|b)
+            return
             ;;
         *)
             echo "无效的选择。请重新选择。"
@@ -51,8 +57,26 @@ execute_script() {
 
 # 主循环
 while true; do
-    show_menu
-    read -p "请输入选项: " choice
-    execute_script $choice
+    show_main_menu
+    read -p "请输入选项: " main_choice
+
+    case $main_choice in
+        1)
+            while true; do
+                show_script_menu
+                read -p "请输入选项: " script_choice
+                execute_script $script_choice
+                echo
+            done
+            ;;
+        Q|q)
+            echo "退出脚本。"
+            exit 0
+            ;;
+        *)
+            echo "无效的选择。请重新选择。"
+            ;;
+    esac
+
     echo
 done
