b158ea34f436e159eee0a51c8df0a0ca8019707c
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2023-12-25 23:06:52 +0800

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 安装 Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# 启动 Docker 服务
+sudo systemctl start docker
+
+# 添加当前用户到 docker 用户组
+sudo usermod -aG docker $USER
+
+# 安装 Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# 验证 Docker 和 Docker Compose 安装
+docker --version
+docker-compose --version
